097573de915bf8489c0d3b78549f1de4bd324684
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-10-10 15:45:54 +0300

    Completed assignemnt

---
+++ index.js
@@ -0,0 +1,15 @@
+/* const receivesAFunction = (spy) => {
+    return spy
+} */
+
+function receivesAFunction(spy) {
+  return spy();
+}
+
+function returnsANamedFunction() {
+  return function test() {};
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {};
+}
